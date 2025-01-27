76c0d57e766e12cff710390e10ab37fbefb883fb
CC <112964687+Pitayafruits@users.noreply.github.com>
2025-01-28 01:11:01 +0800

    feat：项目基础搭建

---
+++ wechat-api/auth-service-88/src/main/java/com/pitayafruits/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.pitayafruits.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public Object hello() {
+        return "Hello World";
+    }
+
+}
