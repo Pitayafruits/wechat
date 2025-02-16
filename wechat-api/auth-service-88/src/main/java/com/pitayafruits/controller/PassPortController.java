c7e78ebaed36c7a865d241f6977f44c368e9a3ca
CC <112964687+Pitayafruits@users.noreply.github.com>
2025-02-16 23:47:24 +0800

    feat：实现获取验证码功能

--- wechat-api/auth-service-88/src/main/java/com/pitayafruits/controller/PassPortController.java
+++ wechat-api/auth-service-88/src/main/java/com/pitayafruits/controller/PassPortController.java
@@ -3,9 +3,11 @@ package com.pitayafruits.controller;
 
 import com.pitayafruits.base.BaseInfoProperties;
 import com.pitayafruits.grace.result.GraceJSONResult;
+import com.pitayafruits.utils.IPUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("passport")
 public class PassPortController extends BaseInfoProperties {
 
-    @GetMapping("generateSMSCode")
-    public GraceJSONResult generateSMSCode(String mobile,
-                                           HttpServletRequest request) {
+    @PostMapping("getSMSCode")
+    public GraceJSONResult getSMSCode(String mobile,
+                                      HttpServletRequest request) {
         if (StringUtils.isBlank(mobile)) {
             return GraceJSONResult.error();
         }
+
+        String requestIp = IPUtil.getRequestIp(request);
+        redis.setnx60s(MOBILE_SMSCODE + ":" +requestIp, mobile);
+
         String code = (int) (Math.random() * 9000 + 1000) + "";
         redis.set(MOBILE_SMSCODE + ":" + mobile, code, 30 * 60);
+
         return GraceJSONResult.ok();
     }
 
