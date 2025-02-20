f1d41672d57ee8aaad9d5591fc71372cf8a16830
CC <112964687+Pitayafruits@users.noreply.github.com>
2025-02-20 23:14:25 +0800

    feat：AOP记录方法执行时间

--- wechat-api/base-service/src/main/java/com/pitayafruits/api/aspect/ServiceLogAspect.java
+++ wechat-api/base-service/src/main/java/com/pitayafruits/api/aspect/ServiceLogAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 @Component
 @Slf4j
@@ -15,15 +16,20 @@ public class ServiceLogAspect {
     @Around("execution(* com.pitayafruits.service.impl..*.*(..))")
     public Object recordTimeLog(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        long begin = System.currentTimeMillis();
+//        long begin = System.currentTimeMillis();
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
 
         Object proceed = joinPoint.proceed();
         String pointName = joinPoint.getTarget().getClass().getName()
                 + "." + joinPoint.getSignature().getName();
 
-        long end = System.currentTimeMillis();
+        long takeTime = stopWatch.getTotalTimeMillis();
 
-        long takeTime = end - begin;
+
+//        long end = System.currentTimeMillis();
+//        long takeTime = end - begin;
 
         if (takeTime > 3000) {
             log.error("{} 执行耗时：{} 毫秒", pointName, takeTime);
