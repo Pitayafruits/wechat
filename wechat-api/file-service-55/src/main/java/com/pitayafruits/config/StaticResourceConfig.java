9fb0c9a55b77a4089933df82f3f1f2a7b9f86f3d
CC <112964687+Pitayafruits@users.noreply.github.com>
2025-02-23 23:26:37 +0800

    feat：静态资源映射

--- wechat-api/file-service-55/src/main/java/com/pitayafruits/config/StaticResourceConfig.java
+++ wechat-api/file-service-55/src/main/java/com/pitayafruits/config/StaticResourceConfig.java
@@ -1,4 +1,4 @@
-package com.pitayafruits.api.config;
+package com.pitayafruits.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
