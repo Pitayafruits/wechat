bb264c2b0d39f80e6cc0f703e8198f8050471931
CC <112964687+Pitayafruits@users.noreply.github.com>
2025-05-05 18:19:01 +0800

    feat：使用redis动态分配Netty集群端口号

--- wechat-api/chat-service-875/src/main/java/com/pitayafruits/netty/ChatServer.java
+++ wechat-api/chat-service-875/src/main/java/com/pitayafruits/netty/ChatServer.java
@@ -1,17 +1,27 @@
 package com.pitayafruits.netty;
 
 
+import com.pitayafruits.netty.utils.JedisPoolUtils;
 import com.pitayafruits.netty.websocket.WSServerInitializer;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * netty 服务启动类
  */
 public class ChatServer {
+
+    public static final Integer nettyDefaultPort = 875;
+    public static final String initOnlineCounts = "0";
 
     public static void main(String[] args) throws InterruptedException {
         // 定义主从线程池
@@ -19,6 +29,10 @@ public class ChatServer {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         // 从线程池处理主线程池交过来的任务
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        // netty服务启动的时候，从redis中查找端口
+        Integer nettyPort = selectPort(nettyDefaultPort);
+
         try {
             // 构建netty服务器
             ServerBootstrap server = new ServerBootstrap();
@@ -26,7 +40,7 @@ public class ChatServer {
                     .channel(NioServerSocketChannel.class) // 设置NIO的双向通道
                     .childHandler(new WSServerInitializer()); // 设置处理器，用于处理workerGroup接受的任务
             // 绑定端口号，启动服务器，启动方式为同步
-            ChannelFuture channelFuture = server.bind(875).sync();
+            ChannelFuture channelFuture = server.bind(nettyPort).sync();
             // 监听关闭的channel
             channelFuture.channel().closeFuture().sync();
         } finally {
@@ -34,5 +48,33 @@ public class ChatServer {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
+    }
+
+    /*
+     * 动态获取端口号
+     */
+    public static Integer selectPort(Integer port) {
+        String portKey = "netty_port";
+        Jedis jedis = JedisPoolUtils.getJedis();
+        jedis.set("jedis-test", "hello world");
+        Map<String, String> portMap = jedis.hgetAll(portKey);
+        System.out.println(portMap);
+        // 由于map中的key都应该是整数类型的port，所以先转换成整数后，再比对，否则string类型的比对会有问题
+        List<Integer> portList = portMap.entrySet().stream()
+                .map(entry -> Integer.valueOf(entry.getKey()))
+                .collect(Collectors.toList());
+        Integer nettyPort = null;
+        if (portList == null || portList.isEmpty()) {
+            jedis.hset(portKey, port+"", initOnlineCounts);
+            nettyPort = port;
+        } else {
+            // 循环portList，获得最大值，并且累加10
+            Optional<Integer> maxInteger = portList.stream().max(Integer::compareTo);
+            Integer maxPort = maxInteger.get().intValue();
+            Integer currentPort = maxPort + 10;
+            jedis.hset(portKey, currentPort+"", initOnlineCounts);
+            nettyPort = currentPort;
+        }
+        return nettyPort;
     }
 }
