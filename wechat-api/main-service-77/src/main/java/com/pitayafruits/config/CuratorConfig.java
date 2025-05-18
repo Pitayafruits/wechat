ed92cfaa9c3b3bed580266c62b54f6f2215d7683
CC <112964687+Pitayafruits@users.noreply.github.com>
2025-05-18 18:46:08 +0800

    feat：监听zk删除节点清理redis中对应端口数据

--- wechat-api/main-service-77/src/main/java/com/pitayafruits/config/CuratorConfig.java
+++ wechat-api/main-service-77/src/main/java/com/pitayafruits/config/CuratorConfig.java
@@ -1,11 +1,16 @@
 package com.pitayafruits.config;
 
+import com.pitayafruits.pojo.netty.NettyServerNode;
+import com.pitayafruits.utils.JsonUtils;
+import com.pitayafruits.utils.RedisOperator;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -23,6 +28,13 @@ public class CuratorConfig {
     private Integer maxRetries; // 最大重试次数
     private String namespace; // 命名空间(root根节点名称)
 
+    @Autowired
+    private RedisOperator redisOperator;
+
+    public static final String PATH = "/server-list";
+
+
+
     @Bean("curatorClient")
     public CuratorFramework curatorClient() {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(
@@ -37,7 +49,47 @@ public class CuratorConfig {
                 .namespace(namespace)
                 .build();
         client.start(); // 启动客户端
+
+        addWatcher(PATH, client);
+
         return client;
+    }
+
+
+    /**
+     * 注册节点的事件监听
+     *
+     * @param path
+     * @param client
+     */
+    public void addWatcher(String path, CuratorFramework client) {
+        CuratorCache curatorCache = CuratorCache.build(client, path);
+        curatorCache.listenable().addListener(((type, oldData, data) -> {
+            switch (type.name()) {
+                case "NODE_CREATED":
+                    log.info("(子)节点创建事件");
+                    break;
+                case "NODE_DELETED":
+                    log.info("(子)节点删除事件");
+
+                    NettyServerNode oldNode =
+                            JsonUtils.jsonToPojo(new String(oldData.getData()),
+                                    NettyServerNode.class);
+
+                    String oldPort = oldNode.getPort() + "";
+                    String portKey = "netty_port";
+
+                    redisOperator.hdel(portKey, oldPort);
+
+                    break;
+                case "NODE_CHANGED":
+                    log.info("(子)节点数据改变事件");
+                    break;
+                default:
+                    log.info("其他事件");
+                    break;
+            }
+        }));
     }
 
 }
